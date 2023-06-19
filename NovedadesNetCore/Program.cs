9e92f771c5e46ad6ada0dafaa6e0b630b2fd1b7a
serraguti <pacoserranox@gmail.com>
2023-06-19 10:32:58 +0200

    commit

--- NovedadesNetCore/Program.cs
+++ NovedadesNetCore/Program.cs
@@ -10,6 +10,14 @@ foreach (Persona p in personas)
 {
     Console.WriteLine(p.IdPersona + " - " + p.Nombre);
 }
+Console.WriteLine("Max y MaxBy Lambda");
+var valor = personas.Max(x => x.Edad);
+Console.WriteLine("Valor Max: " + valor);
+//NOVEDAD ES UTILIZAR MAXBY, QUE DEVUELVE EL OBJETO
+var dato = personas.MaxBy(x => x.Edad);
+Console.WriteLine("Dato MaxBy: " + dato.Nombre);
+
+
 Console.WriteLine("Filtrando por edad");
 List<Persona> personasFiltro = helperLambda.GetPersonasEdad();
 foreach (Persona p in personasFiltro)
