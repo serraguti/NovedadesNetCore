0daf3e775a69c7f6c179651cc86dcbdd692b71ef
serraguti <pacoserranox@gmail.com>
2023-06-19 10:24:43 +0200

    Add project files.

---
+++ NovedadesNetCore/Helpers/HelperLambda.cs
@@ -0,0 +1,64 @@
+﻿using NovedadesNetCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovedadesNetCore.Helpers
+{
+    public class HelperLambda
+    {
+        private List<Persona> personasList;
+
+        public HelperLambda()
+        {
+            this.personasList = new List<Persona>
+            {
+                new Persona {IdPersona = 1, Nombre = "Lucia", Email= "lucia@gmail.com", Edad=21},
+                new Persona {IdPersona = 2, Nombre = "Adrian", Email= "adrian@gmail.com", Edad=25},
+                new Persona {IdPersona = 3, Nombre = "Carlos", Email= "carlos@gmail.com", Edad=28},
+                new Persona {IdPersona = 4, Nombre = "Antonia", Email= "antonia@gmail.com", Edad=51},
+                new Persona {IdPersona = 5, Nombre = "Diana", Email= "diana@gmail.com", Edad=31},
+                new Persona {IdPersona = 6, Nombre = "Pedro", Email= "pedro@gmail.com", Edad=11}
+            };
+        }
+
+        public List<Persona> GetPersonas()
+        {
+            return this.personasList;
+        }
+
+        public List<Persona> GetPersonasEdad()
+        {
+            List<Persona> outputList = new List<Persona>();
+            foreach (Persona p in this.personasList)
+            {
+                if (p.Edad >= 20 && p.Edad <= 30)
+                {
+                    outputList.Add(p);
+                }
+            }
+            return outputList;
+        }
+
+        public List<Persona> GetPersonasEdadLambda(int edadInicial, int edadFinal)
+        {
+            List<Persona> outputList =
+                this.personasList.Where(x => x.Edad >= edadInicial && x.Edad <= edadFinal).ToList();
+            return outputList;
+        }
+
+        public List<Persona> GetPersonasPatronPropiedades() // (int edadInicial, int edadFinal)
+        {
+            List<Persona> outputList =
+                this.personasList.FindAll(p => p is
+                {
+                    Edad: >= 20,
+                    Edad: <= 30, 
+                    Email.Length: > 0
+                });
+            return outputList;
+        }
+    }
+}
---
+++ NovedadesNetCore/Program.cs
@@ -0,0 +1,30 @@
+﻿using NovedadesNetCore.Helpers;
+using NovedadesNetCore.Models;
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Ejemplo novedades");
+Console.WriteLine("Lambda");
+HelperLambda helperLambda = new HelperLambda();
+List<Persona> personas = helperLambda.GetPersonas();
+foreach (Persona p in personas)
+{
+    Console.WriteLine(p.IdPersona + " - " + p.Nombre);
+}
+Console.WriteLine("Filtrando por edad");
+List<Persona> personasFiltro = helperLambda.GetPersonasEdad();
+foreach (Persona p in personasFiltro)
+{
+    Console.WriteLine(p.IdPersona + " - " + p.Nombre + ", Edad: " + p.Edad);
+}
+Console.WriteLine("Filtrando por edad Lambda");
+List<Persona> personasFiltroLambda = helperLambda.GetPersonasEdadLambda(20, 40);
+foreach (Persona p in personasFiltroLambda)
+{
+    Console.WriteLine(p.IdPersona + " - " + p.Nombre + ", Edad: " + p.Edad);
+}
+Console.WriteLine("Filtrando por edad Patrón propiedades");
+List<Persona> personasFiltroPatron = helperLambda.GetPersonasPatronPropiedades();
+foreach (Persona p in personasFiltroPatron)
+{
+    Console.WriteLine(p.IdPersona + " - " + p.Nombre + ", Edad: " + p.Edad);
+}
