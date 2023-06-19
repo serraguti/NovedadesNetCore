b6cdab4ec3342002f3ec12612faef7805b31a10f
serraguti <pacoserranox@gmail.com>
2023-06-19 11:09:44 +0200

    commit

---
+++ NovedadesNetCore/Helpers/HelperFiles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovedadesNetCore.Helpers
+{
+    public static class HelperFiles
+    {
+        //METODO PARA LEER EL CONTENIDO DE UN FICHERO
+        public static async Task<string> ReadFileAsync(string path)
+        {
+            string data = await File.ReadAllTextAsync(path);
+            return data;
+        }
+    }
+}
--- NovedadesNetCore/Program.cs
+++ NovedadesNetCore/Program.cs
@@ -1,21 +1,24 @@
-﻿using NovedadesNetCore.Helpers;
-using NovedadesNetCore.Models;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 
 Console.WriteLine("Ejemplo novedades");
+Console.WriteLine("Ejemplo async/await");
+string path = @"C:\Users\Serra\Documents\CEDEX\Contacto Curso Cedex.txt";
+string data = await HelperFiles.ReadFileAsync(path);
+Console.WriteLine(data);
+
 Console.WriteLine("Lambda");
 HelperLambda helperLambda = new HelperLambda();
-List<int> numeros = helperLambda.GetElementosPaginadosTake(0,5);
+List<int> numeros = helperLambda.GetElementosPaginadosTake(0, 5);
 foreach (int num in numeros)
 {
     Console.WriteLine(num);
 }
-Console.WriteLine("Siguientes...");
-List<int> numeros1 = helperLambda.GetElementosPaginadosTake(5, 10);
-foreach (int num in numeros1)
-{
-    Console.WriteLine(num);
-}
+//Console.WriteLine("Siguientes...");
+//List<int> numeros1 = helperLambda.GetElementosPaginadosTake(5, 10);
+//foreach (int num in numeros1)
+//{
+//    Console.WriteLine(num);
+//}
 //List<int> numeros = helperLambda.GetElementosPaginados(0,5);
 //foreach (int num in numeros)
 //{
@@ -31,11 +34,11 @@ foreach (int num in numeros1)
 
 
 
-//List<Persona> personas = helperLambda.GetPersonas();
-//foreach (Persona p in personas)
-//{
-//    Console.WriteLine(p.IdPersona + " - " + p.Nombre);
-//}
+List<Persona> personas = helperLambda.GetPersonas();
+foreach (Persona p in personas)
+{
+    Console.WriteLine(p.IdPersona + " - " + p.Nombre);
+}
 //Console.WriteLine("Max y MaxBy Lambda");
 //var valor = personas.Max(x => x.Edad);
 //Console.WriteLine("Valor Max: " + valor);
